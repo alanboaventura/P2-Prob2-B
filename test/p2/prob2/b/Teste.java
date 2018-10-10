2228cc3966fea49a469a1c08a48cab40073f94a2
Alan Boaventura <alanboaventurab@gmail.com>
2018-10-10 19:54:10 -0300

    V1 Teste

--- test/p2/prob2/b/Teste.java
+++ test/p2/prob2/b/Teste.java
@@ -1,13 +1,18 @@
-package p2.prob2.b;
+﻿package p2.prob2.b;
 
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 public class Teste {
+
+    public Teste() {
+    }
 
     ContaCorrente contaCorrente1;
     ContaCorrente contaCorrente2;
 
-    public void before() {
+    @Before
+    public void setUp() {
         ClientePessoaFisica clientePessoaFisica = new ClientePessoaFisica("Teste 1", "334", "777", "999");
         ClientePessoaJuridica clientePessoaJuridica = new ClientePessoaJuridica("Teste 2", "221", "888", "556", "1.1.1.1");
 
@@ -17,69 +22,19 @@ public class Teste {
         contaCorrente2 = new ContaCorrente(clientePessoaJuridica, 3565, 22388, true, true, true, true, true, true);
         contaCorrente2.depositar(500);
         clientePessoaJuridica.addConta(contaCorrente2);
+        
+        ServicoBaixaInvestimento servicoBaixaInvestimento1 = new ServicoBaixaInvestimento(contaCorrente1);
+        ServicoFluxoCaixa servicoFluxoCaixa1 = new ServicoFluxoCaixa(contaCorrente1);
+        ServicoNotificacoes servicoNotificacoes1 = new ServicoNotificacoes(contaCorrente1);
+
+        ServicoBaixaInvestimento servicoBaixaInvestimento2 = new ServicoBaixaInvestimento(contaCorrente2);
+        ServicoFluxoCaixa servicoFluxoCaixa2 = new ServicoFluxoCaixa(contaCorrente2);
+        ServicoNotificacoes servicoNotificacoes2 = new ServicoNotificacoes(contaCorrente2);
     }
 
     @Test
-    public void testeSaque() {
+    public void saque() {
+        contaCorrente1.sacar(50);
+        // TODO Dever de casa, e fazer pras demais operações também.
     }
-
-//    @Test
-//    public void testCadastraCliente_PessoaFisica() {
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("Cadastra cliente e conta");
-//        ClientePessoaFisica grupoB = new ClientePessoaFisica("grupoB", "4002-8922", "1234567890", "1793248650");
-//        ContaCorrente conta = new ContaCorrente(3, 0);
-//        conta.setCliente(grupoB);
-//        System.out.println("Valor inicial da conta de " + grupoB.getNome() + " é de: " + conta.getSaldo());
-//    }
-//
-//    @Test
-//    public void testCadastraCliente_PessoaFisica_RealizaOperacoes() {
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("Realiza serviços na conta da pessoa física");
-//        System.out.println("Cadastra cliente");
-//        ClientePessoaFisica grupoB = new ClientePessoaFisica("grupoB", "4002-8922", "1234567890", "1793248650");
-//        ContaCorrente conta = new ContaCorrente(3, 0);
-//        ContaCorrente conta2 = new ContaCorrente(3, 0);
-//        conta.setCliente(grupoB);
-//        System.out.println("Valor inicial da conta de " + grupoB.getNome() + " é de: " + conta.getSaldo());
-//        System.out.println("Deposita: 100");
-//        conta.depositar(100);
-//        System.out.println("" + conta.realizado.get(0));
-//        System.out.println("Valor na conta é de: " + conta.getSaldo());
-//        System.out.println("Saca: 50");
-//        conta.sacar(50);
-//        System.out.println("" + conta.realizado.get(1));
-//        System.out.println("Valor na conta é de: " + conta.getSaldo());
-//        System.out.println("Transfere 20 para a conta2");
-//        conta.transferir(20, conta2);
-//        System.out.println("" + conta.realizado.get(2));
-//        System.out.println("Valor na conta2 é de: " + conta2.getSaldo());
-//        System.out.println("Valor na conta é de: " + conta.getSaldo());
-//    }
-//
-//
-//    @Test
-//    public void testCadastraCliente_PessoaJuridica_EscolheServiços() {
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("Seleciona serviços da conta de Pessoa Jurídica");
-//        ClientePessoaJuridica grupoB = new ClientePessoaJuridica("grupoB", "123321", "99999999", "44444444", "www.servidor.com.br");
-//        ContaCorrente conta = new ContaCorrente(5, 6);
-//        conta.setCliente(grupoB);
-//
-//    }
-//
-//    @Test
-//    public void testCadastraCliente_PessoaFisica_EscolheServiços() {
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("Seleciona serviços da conta de Pessoa Física");
-//        ClientePessoaFisica grupoB = new ClientePessoaFisica("grupoB", "4002-8922", "1234567890", "1793248650");
-//        ContaCorrente conta = new ContaCorrente(3, 0);
-//        conta.setCliente(grupoB);
-//        System.out.println("Valor inicial da conta de " + grupoB.getNome() + " é de: " + conta.getSaldo());
-//    }
 }
